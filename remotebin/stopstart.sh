6b0976e806520639bb25cd31a2d6b2db866dbd2c
tonylampada <tonylampada@gmail.com>
2017-08-09 16:34:31 -0300

    mapear dkdata no stopstart

--- remotebin/stopstart.sh
+++ remotebin/stopstart.sh
@@ -18,11 +18,12 @@ dkpull() {
 
 dkstopstart(){
     containername=${app}_${environ}_${service}
+    dkdata=~/dockerdata/${app}_${environ}
     image=$app:$environ
     envfile=~/${app}_${environ}.env
     docker stop $containername
     docker rm $containername
-    docker run -d --restart=unless-stopped --name=$containername --env-file=$envfile $image $cmd
+    docker run -d --restart=unless-stopped --name=$containername --env-file=$envfile -v $dkdata:/dkdata $image $cmd
     exitcode=$?
     return $exitcode
 }
