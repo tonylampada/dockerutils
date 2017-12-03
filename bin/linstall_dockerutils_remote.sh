a090cbda2433735fde1a6dc152ebf71adbcf5ba8
tonylampada <tonylampada@gmail.com>
2017-12-03 11:05:39 -0200

    bugfix

--- bin/linstall_dockerutils_remote.sh
+++ bin/linstall_dockerutils_remote.sh
@@ -6,7 +6,7 @@ source $SCRIPTPATH/common.sh
 
 set -e
 ssh -o StrictHostKeyChecking=no $host mkdir -p dockerutils
-rsync -L -av $SCRIPTPATH/remotebin $host:./dockerutils/
+rsync -L -av $SCRIPTPATH/../remotebin $host:./dockerutils/
 if [ -d ~/.dockerutils ]; then
     rsync -av ~/.dockerutils $host:./
 fi
