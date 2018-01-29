4b2f6ca204b43285ad19e855718465af705963f2
tonylampada <tonylampada@gmail.com>
2018-01-29 14:40:44 -0200

    sem restart

--- remotebin/stopstart.sh
+++ remotebin/stopstart.sh
@@ -17,7 +17,7 @@ dkstopstart(){
     docker stop $containername || true
     docker rm $containername || true
     echo "stopstart with $DKPARAMS"
-    docker run $DKPARAMS -d --restart=unless-stopped --name=$containername --env-file=$envfile -v $dkdata:/dkdata $image start_${service}.sh
+    docker run $DKPARAMS -d --name=$containername --env-file=$envfile -v $dkdata:/dkdata $image start_${service}.sh
     exitcode=$?
     return $exitcode
 }
