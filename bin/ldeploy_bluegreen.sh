865687ef301393fdc02eb72d3b3d6f7d6699b98f
Iuri de Silvio <iurisilvio@gmail.com>
2020-03-04 13:23:01 -0300

    Faz o deploy bluegreen em paralelo

--- bin/ldeploy_bluegreen.sh
+++ bin/ldeploy_bluegreen.sh
@@ -7,6 +7,23 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 source $SCRIPTPATH/common.sh
 source_app_env $app $environ
 
+function vaibluegreen(){
+    host=$1
+    set -e
+    ssh -o StrictHostKeyChecking=no $host dockerutils/remotebin/$cmd $app $version $environ
+    echo "[BLUEGREEN] deploy feito no host $host"
+
+}
+
+function waitpidsaveresult() {
+  p=$1
+  i=$2
+  set +e
+  wait $p
+  excods[$i]=$?
+  set -e
+}
+
 set -e
 if [ "$HOSTS" ]; then
     hosts="$HOSTS"
@@ -19,10 +36,28 @@ cmd=bluegreen.sh
 if [ "$SLOW" == "1" ]; then
 	cmd=bluegreen_slow.sh
 fi
+
 if [ "$hosts" ]; then
+    pids=()
+    excods=()
+    ahosts=($hosts)
+
     echo "[BLUEGREEN] hosts pra deploy: $hosts"
+
+    i=0
     for host in $hosts; do
-        ssh -o StrictHostKeyChecking=no $host dockerutils/remotebin/$cmd $app $version $environ
-        echo "[BLUEGREEN] deploy feito no host $host"
+        echo "[BLUEGREEN] iniciando em $host"
+        vaibluegreen $host & pids[$i]=$!
+        i=$(($i+1))
+    done
+    for i in "${!pids[@]}"; do 
+        waitpidsaveresult ${pids[$i]} $i
+    done
+
+    for i in "${!pids[@]}"; do 
+        if [ ${excods[$i]} != "0" ]; then
+          echo "vaibluegreen no host ${ahosts[$i]} saiu com codigo ${excods[$i]}"
+            exit ${excods[$i]}
+        fi
     done
 fi
